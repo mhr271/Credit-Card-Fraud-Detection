0f13acc44161b571de0ca9362f39e3bc490db9cf
mhr271 <humaidirahman271@gmail.com>
2026-07-13 15:36:54 +1000

    Update model.py

--- model.py
+++ model.py
@@ -9,3 +9,11 @@ from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.ensemble import RandomForestRegressor
 import matplotlib.pyplot as plt
+
+df = pd.read_csv("creditcard.csv")
+print(df)
+
+y = df["class"]
+x = df.drop("class",axis=1)
+
+x_train , x_test, y_train , y_test = train_test_split(test_size=0.2, random_state=42 , stratify = y)
