b1b1f3740cdd704fa7da1dd39185865481599285
mhr271 <humaidirahman271@gmail.com>
2026-07-13 15:57:46 +1000

    Update model.py

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import pandas as pd 
 import numpy as np 
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import mean_squared_error,r2_score
 from sklearn.preprocessing import StandardScaler
@@ -13,7 +13,21 @@ import matplotlib.pyplot as plt
 df = pd.read_csv("creditcard.csv")
 print(df)
 
-y = df["class"]
-x = df.drop("class",axis=1)
+y = df["Class"]
+x = df.drop("Class",axis=1)
 
-x_train , x_test, y_train , y_test = train_test_split(test_size=0.2, random_state=42 , stratify = y)
+x_train , x_test, y_train , y_test = train_test_split(x,y
+    ,test_size=0.2, random_state=42 , stratify=y)
+
+print(x_train.shape,x_test.shape,y_train.shape,y_test.shape)
+
+lr = Pipeline(
+    steps=[
+    ('scale',StandardScaler()),
+    ('model', LogisticRegression(class_weight='balanced',max_iter=200))
+    ]
+)
+lr.fit(x_train,y_train)
+
+lr_y_train_pred = lr.predict(x_train)
+lr_y_test_pred = lr.predict(x_test)
